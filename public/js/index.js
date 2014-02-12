f4cd29f76e583b8fa3cdf4640cb2eeaadcb1bc60
Danielle <dinkydani@googlemail.com>
2014-02-12 15:51:17 +0000

    playing with pie chart colours

--- public/js/index.js
+++ public/js/index.js
@@ -115,7 +115,7 @@ d3.json('/findAll', function (data){
         .radius(80)
         .dimension(countryDimension)
         .group(countryGroup)
-        .ordinalColors(["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99"])
+        .colors(d3.scale.category20c())
         .title(function (d){
             return d.key + ": " + d.value + " (" + Math.floor(d.value / all.value() * 100) + "%)";
         })
